eb3f0a9b9e4955f1144cf0364dbf66848b7b1b62
cpottamus <cp3192@gmail.com>
2014-05-13 20:38:56 -0500

    yeahhhhh first commit

---
+++ assets.gen.d
@@ -0,0 +1,55 @@
+assets.gen.o: assets.gen.cpp \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/group.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/syscall.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/types.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/audio.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/events.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/vram.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/asset.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/elf.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/limits.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/macros.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/math.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/loader.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/array.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/memory.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/audio.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/image.h
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/group.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/syscall.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/types.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/audio.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/events.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/vram.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/asset.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/elf.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/limits.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/macros.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/math.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/loader.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/array.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/memory.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/audio.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/image.h:
---
+++ main.d
@@ -0,0 +1,121 @@
+main.o: main.cpp \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/syscall.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/types.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/audio.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/events.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/vram.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/asset.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/elf.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/array.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/memory.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/macros.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/group.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/limits.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/math.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/loader.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/audio.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/image.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/audio.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/bluetooth.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/cube.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/event.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/filesystem.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/metadata.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/motion.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/string.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/system.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/time.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/usb.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/color.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/sprite.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/framebuffer.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/bg0rom.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/bg0.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/bg1.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/bg2.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/tilebuffer.h \
+  assets.gen.h
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/syscall.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/types.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/audio.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/events.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/vram.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/asset.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/abi/elf.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/array.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/memory.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/macros.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/group.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/limits.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/math.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/loader.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/audio.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/asset/image.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/audio.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/bluetooth.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/cube.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/event.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/filesystem.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/metadata.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/motion.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/string.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/system.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/time.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/usb.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/color.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/sprite.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/framebuffer.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/bg0rom.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/bg0.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/bg1.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/bg2.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/USB_Sifteo/include/sifteo/video/tilebuffer.h:
+
+assets.gen.h:
